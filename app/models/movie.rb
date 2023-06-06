8f3fba697bb9700cd48700bbed2b8d0e93e0f6dc
Tan Hao Yi Yi <1006337@mymail.sutd.edu.sg>
2023-06-06 04:46:08 +0000

    part 1 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,14 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+    ['G','PG','PG-13','R']
+  end
+  def self.with_ratings(ratings_list)
+    if ratings_list.present?
+      formatted_ratings_list = ratings_list.map { |rating| rating.upcase }
+      Movie.where(rating: formatted_ratings_list)
+    else
+      Movie.all
+    end
+    
+  end
 end
